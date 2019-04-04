f3767aa35ee4addf174c997df746e643d388dd3c
Yifeng <yiz569@ucsd.edu>
2019-04-04 14:54:52 -0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, I am Yifeng Zhang.'
+  document.getElementById('main-content').innerHTML = 'Hello, I am Yifeng Zhang. I am currently building this f*** webpage.'
 }
