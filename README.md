16809e42f48c2ca592b00ffe2e24d15972fa9b91
Yifeng <yiz569@ucsd.edu>
2021-01-27 15:02:50 +0800

    static

--- README.md
+++
@@ -1,5 +0,0 @@
-# Yifeng Zhang Website
-This repository contains the source code of my personal website
-which is built with React.
-<br>
-Check out the website <a href="http://www.yifengzhang.com">HERE!</a>
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { unregister } from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
